97da3a40414ef94e9210d0e4ea9f9d358d5a53da
Tyler Durrett <hi@tylerdurrett.com>
2025-09-01 19:43:54 -0500

    Wrap Image component in a Link to navigate to the reading page

--- src/app/(with-nav)/page.tsx
+++ src/app/(with-nav)/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
   title: 'Robo Inspo | Slop Review',
@@ -8,12 +9,14 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="w-screen h-screen relative">
-      <Image
-        src="/static/opengraph.jpg"
-        alt="Robo Inspo"
-        fill
-        className="object-fill"
-      />
+      <Link href="/reading">
+        <Image
+          src="/static/opengraph.jpg"
+          alt="Robo Inspo"
+          fill
+          className="object-fill"
+        />
+      </Link>
     </div>
   )
 }
